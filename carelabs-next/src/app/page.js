83a9f28d0ecd738bef1ecb93c241cb5171a77ee4
SreyasTest <test.sreyas@gmail.com>
2025-11-21 16:03:02 +0530

    create Service Page and update home page4

--- carelabs-next/src/app/page.js
+++ carelabs-next/src/app/page.js
@@ -54,9 +54,9 @@ console.log("Home Data",homeData);
 if (!homeData) return null;
   return (
     <>
-   <div className="home-cover relative top-[80px] w-full min-h-[calc(100vh-80px)] 2xl:p-4 flex flex-col items-center justify-center">
+   <div className="home-cover relative top-[80px] w-full min-h-[calc(100vh-80px)]  flex flex-col items-center justify-center">
   {/* Section 1 */}
-  <div className="w-11/12 md:w-4/5 lg:w-3/5 h-auto md:h-[85%] 2xl:w-[65%]  bg-gray-50 rounded-3xl flex flex-col items-center justify-center navbar-shadow p-4 md:p-6">
+  <div className="w-11/12 md:w-4/5 lg:w-3/5 h-auto md:h-[85%] 2xl:h-[85%] 2xl:w-[65%]  bg-gray-50 rounded-3xl flex flex-col items-center justify-center navbar-shadow p-4 md:p-6">
     <div className="w-full flex flex-col items-center justify-evenly space-y-6 md:space-y-8">
 
       {/* Heading Button */}
