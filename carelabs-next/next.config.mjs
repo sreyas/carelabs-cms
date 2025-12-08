4d305eee55e3dab2a1fd90217e0557ddfcc2d0ce
SreyasTest <test.sreyas@gmail.com>
2025-12-08 17:16:02 +0530

    update next.config.mjs and pakage.json

--- carelabs-next/next.config.mjs
+++ carelabs-next/next.config.mjs
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactCompiler: true,          // Enable new React compiler (good)
-  swcMinify: true,              // Minifies output (good)
-
-  browserslist: {
-    production: [
-      "last 2 chrome versions",
-      "last 2 edge versions",
-      "last 2 safari versions",
-      "not dead",
-      "not IE 11",
-      "not chrome < 90",
-      "not safari < 13"
-    ],
-    development: [
-      "last 1 chrome version",
-      "last 1 firefox version",
-      "last 1 safari version"
-    ]
-  },
-
-  // Prevent Next from sending polyfills for old browsers
-  experimental: {
-    forceSwcTransforms: true,
-    optimizePackageImports: ["lucide-react"],
-  },
+  
+  /* config options here */
+  reactCompiler: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
